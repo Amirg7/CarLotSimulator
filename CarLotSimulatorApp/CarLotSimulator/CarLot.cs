52d07238e8376237c796219cc8b0a9a900b3084b
Amir Gaffar <AmirG7@yahoo.com>
2020-12-10 12:09:24 -0500

    Added Static Class

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -7,7 +7,11 @@ namespace CarLotSimulator
     {
         public CarLot()
         {
+
         }
+
+
+        public static int numberOfCars = 1;
 
         public List<Car> Cars = new List<Car>();
 
